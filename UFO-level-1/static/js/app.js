9b99175e8408078c1d13a51b61ebed9315061a97
Ben Pharn <benjaminpharn@gmail.com>
2020-09-27 22:14:30 -0700

    table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Import data and make table
+tableData.forEach((ufoSighting) => {
+  // append row for each entry
+  var row = tbody.append("tr");
+  Object.entries(ufoSighting).forEach(([key, value]) => {
+    // append cell for each datapoint   
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
