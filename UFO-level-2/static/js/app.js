2f3e0f493b40ce973124dbc99e0359c8da2d2c81
Ben Pharn <benjaminpharn@gmail.com>
2020-09-27 22:25:53 -0700

    init for level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,38 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Import data and make table
+tableData.forEach((ufoSighting) => {
+  // append row for each entry
+  var row = tbody.append("tr");
+  Object.entries(ufoSighting).forEach(([key, value]) => {
+    // append cell for each datapoint   
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+// Button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+    tbody.html("");
+
+    // select user input
+    var userInput = d3.select("#datetime");
+    var userInputValue = userInput.property("value");
+    // create filter
+    var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === userInputValue);
+
+    filteredData.forEach((selections) => {
+        var row = tbody.append("tr");
+        Object.entries(selections).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value); 
+        });
+    
+    });
+});
+
