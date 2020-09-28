a22aee4a45833160b824dc05c2b5039cdbaa54af
Ben Pharn <benjaminpharn@gmail.com>
2020-09-27 23:12:33 -0700

    part 2 finished

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -21,10 +21,37 @@ button.on("click", function() {
     tbody.html("");
 
     // select user input
+    // added for multiple user inputs
     var userInput = d3.select("#datetime");
-    var userInputValue = userInput.property("value");
+    var userInputDate = userInput.property("value");
+    var userInput = d3.select("#city");
+    var userInputCity = userInput.property("value");
+    var userInput = d3.select("#state");
+    var userInputState = userInput.property("value");
+    var userInput = d3.select("#country");
+    var userInputCountry = userInput.property("value");
+    var userInput = d3.select("#shape");
+    var userInputShape = userInput.property("value");
+
+    filteredData = tableData
     // create filter
-    var filteredData = tableData.filter(ufoSighting => ufoSighting.datetime === userInputValue);
+    // if statements needed because if empty, breaks table
+    if (userInputDate) {
+    var filteredData = filteredData.filter(ufoSighting => ufoSighting.datetime === userInputDate);
+    };
+    if (userInputCity) {
+        var filteredData = filteredData.filter(ufoSighting => ufoSighting.city === userInputCity);
+    };
+    if (userInputState) {
+        var filteredData = filteredData.filter(ufoSighting => ufoSighting.state === userInputState);
+    };
+    if (userInputCountry) {
+        var filteredData = filteredData.filter(ufoSighting => ufoSighting.country === userInputCountry);
+    };       
+    if (userInputShape) {
+        var filteredData = filteredData.filter(ufoSighting => ufoSighting.shape === userInputShape);
+    };
+
 
     filteredData.forEach((selections) => {
         var row = tbody.append("tr");
